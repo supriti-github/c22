3da838d8850e3baf6aa7ce7ff14c5b32e70b29b6
supriti-github <75122616+supriti-github@users.noreply.github.com>
2020-12-11 12:25:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var myEngine, myWorld;
+var ground, ball;
+
+function setup() {
+  createCanvas(400,400);
+  myEngine =Engine.create();
+  myWorld = myEngine.world;
+
+  var ground_options = {
+    isStatic: true
+
+  }
+  var ball_options = {
+    
+    restitution : 1.0
+  }
+
+
+  ground = Bodies.rectangle(200,380,50,50,ground_options);
+  World.add(myWorld,ground);
+
+  ball = Bodies.circle(200,200,25,ball_options);
+  World.add(myWorld,ball);
+  
+  console.log(ball);
+
+}
+
+function draw() {
+  background("orange"); 
+  Engine.update(myEngine)
+
+  rectMode(CENTER);
+  rect(ground.position.x,ground.position.y,400,20); 
+  ellipseMode(RADIUS);
+  ellipse(ball.position.x,ball.position.y,50,50);
+} 
